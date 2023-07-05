d9c8fcb723f6de8d4c2691b73afffae2b5d79529
Xiaoju_Hu <111913190+xh217@users.noreply.github.com>
2023-07-05 13:30:21 -0400

    Create coor_dataframe.R

---
+++ coor_dataframe.R
@@ -0,0 +1,26 @@
+coor_dataframe<-function(coordinates){
+matches <- regmatches(coordinates, gregexpr("\\d+", coordinates))[[1]]
+
+# Extract chromosome number and positions for first junction block
+first_chromosome <- matches[1]
+first_start_position <- as.numeric(matches[2])*1000
+first_end_position <- as.numeric(matches[2])*1000 + window
+
+# Extract chromosome number and positions for second junction block
+sencond_chromosome <- matches[3]
+second_start_position <- as.numeric(matches[4])*1000
+second_end_position<- as.numeric(matches[4])*1000 + window
+
+# Construct dataframe
+first_junction <- data.frame(chr = paste0("chr",first_chromosome),
+                    start = first_start_position,
+                    end = first_end_position
+                    )
+second_junction <- data.frame(chr = paste0("chr",second_chromosome),
+                    start = second_start_position,
+                    end = second_end_position
+                    )  
+res<-list(first_junction, second_junction)
+                     
+return(res)                                    
+}                    
